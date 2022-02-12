7c0e45fc82eaf81fa7ce38f2d054a227fd415d76
Fabio Ardemagni <96312583+fabcreative@users.noreply.github.com>
2022-02-12 00:02:14 +0000

    comit

--- gradient.js
+++ gradient.js
@@ -23,11 +23,9 @@ function init() {
 	// document.getElementById('canvas').appendChild(renderer.domElement);
 	document.body.appendChild( renderer.domElement );
 
-
-	
 	scene = new THREE.Scene();
 	
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+	camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 1000);
 	camera.position.z = 400;
 	scene.add(camera);
 	
